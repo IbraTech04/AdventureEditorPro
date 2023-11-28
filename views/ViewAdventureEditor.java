32b78f99d63fd75d1080f10eab24ab478d34a9df
abby.yanku <abigail.yanku@mail.utoronto.ca>
2023-11-27 23:31:08 -0500

    [DEV-2.1] Made all buttons in ViewAdventureEditor accessible.

--- views/ViewAdventureEditor.java
+++ views/ViewAdventureEditor.java
@@ -124,6 +124,7 @@ public class ViewAdventureEditor {
         roomViewHbox.setPrefHeight(20);
         Label roomsLabel = new Label("All Rooms:  ");
         addRoomButton = new Button("Add Room");
+        makeButtonAccessible(addRoomButton, "Add Room", "Add a Room to your Game.", "Click to add a Room to your Game.");
         addRoomButton.setOnAction(e -> {
             controller.addRoom();
         });
@@ -151,6 +152,7 @@ public class ViewAdventureEditor {
         HBox gatesLabelButton = new HBox();
         //Create Visualize Button
         visualizeButton = new Button("Visualize");
+        makeButtonAccessible(visualizeButton, "Visualize", "Visualize the gates from the current room.", "Click to visualize the gates from the current room.");
         visualizeButton.setOnAction(e -> handleVisualize());
         //Add Gates Label and Visualize Button to HBox
         gatesLabelButton.setPadding(new Insets(5, 5, 5, 5));
@@ -240,6 +242,7 @@ public class ViewAdventureEditor {
         HBox imageButtonLabel = new HBox();
         imageLabel = new Label("");
         imageButton = new Button("Add Image");
+        makeButtonAccessible(imageButton, "Add Image", "Add Image an Image to your Room.", "Click to add an image from your computer to your Room.");
         imageButton.setOnAction(e -> handleAddImage());
         imageButtonLabel.getChildren().addAll(imageButton, imageLabel);
 
@@ -253,9 +256,11 @@ public class ViewAdventureEditor {
 
         //Add Object addition button
         addObjectButton = new Button("Add Object");
+        makeButtonAccessible(addObjectButton, "Add Object", "Add an Object to your Room.", "Click to add an Object to the current Room.");
         addObjectButton.setOnAction(e -> handleAddObjectButton());
         //Add Gate addition button
         addGateButton = new Button("Add Gate");
+        makeButtonAccessible(addGateButton, "Add Gate", "Add a Gate to your Room.", "Click to add a Gate to the current Room.");
         addGateButton.setOnAction(e -> handleAddGateButton());
 
         //Add Forced Check Box
@@ -278,6 +283,7 @@ public class ViewAdventureEditor {
         //Create Run Button
         HBox runButtonBox = new HBox();
         runButton = new Button("Run Game!");
+        makeButtonAccessible(runButton, "Run Game", "Run the Game you have created.", "Click to run the Game you have created.");
         runButton.setOnAction(e -> handleRun());
         runButtonBox.getChildren().add(runButton);
         runButtonBox.setAlignment(Pos.CENTER);
@@ -348,6 +354,7 @@ public class ViewAdventureEditor {
             trashIconView.setFitWidth(30);
             trashIconView.setFitHeight(30);
             Button deleteButton = new Button();
+            makeButtonAccessible(deleteButton, "Delete Room", "Delete this room.", "Click to delete this Room.");
             // TODO: change currently selected room upon deletion if needed
             deleteButton.setOnAction(e -> controller.deleteRoom(room));
             deleteButton.setGraphic(trashIconView);
@@ -358,6 +365,7 @@ public class ViewAdventureEditor {
             editIconView.setFitWidth(30);
             editIconView.setFitHeight(30);
             Button editButton = new Button();
+            makeButtonAccessible(editButton, "Edit Room", "Edit this room.", "Click to edit this Room.");
             editButton.setOnAction(e -> handleEdit());
             editButton.setGraphic(editIconView);
 
