cb0790e2b63411f5dc6c3f5ee3fd85a83ba9a613
Faizaan Bhemat <faizaan.bhemat@mail.utoronto.ca>
2023-12-01 22:41:39 -0500

    Added a TTS interface and class using FreeTTS. Please Test WelcomeController and Helpview to check whether this works. My JavaFX is not working :(

--- views/HelpView.java
+++ views/HelpView.java
@@ -5,7 +5,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
-
+import Free_TTS.*;
 import java.io.IOException;
 
 /**
@@ -16,6 +16,7 @@ import java.io.IOException;
 public class HelpView {
 
     public HelpView() {
+        TTS tts = new TTS();
         Stage stage = new Stage();
         FXMLLoader loader = new FXMLLoader(getClass().getResource("HelpView.fxml"));
         Parent root = null;
@@ -32,5 +33,13 @@ public class HelpView {
         // Disable Resizing
         stage.setResizable(false);
         stage.show();
+        tts.speak("Instructions:\n" +
+                "\n" +
+                "This is an Adventure Game Creator.\n" +
+                "To create an Adventure Game you can start by creating rooms.\n" +
+                "You can add a room by clicking on the Create Room button. You can add gates to your room by pressing the add Gate button.\n" +
+                "You must create a start and end room. Some of your rooms can force teleport a player to another room. Some rooms are also blocked by Objects.\n" +
+                "To create an object, press the create object button. You can add pictures to objects and rooms. \n" +
+                "After you are done, press  finish and you will be able to play the game you create. Enjoy!");
     }
 }
--- views/WelcomeController.java
+++ views/WelcomeController.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
+import Free_TTS.*;
 
 import java.io.File;
 
@@ -22,11 +23,13 @@ public class WelcomeController {
      * @param actionEvent the event that triggered this method
      * @author Ibrahim Chehab
      */
+    TTS tts = new TTS();
 
 
 
     @FXML
     public void createProj(ActionEvent actionEvent) {
+        tts.speak("Create Project");
         System.out.print("New Game button pressed");
     }
 
@@ -42,6 +45,7 @@ public class WelcomeController {
     @FXML
     public void loadProj(ActionEvent actionEvent) {
         // Open a folder selection dialog
+        tts.speak("Load Project");
         DirectoryChooser directoryChooser = new DirectoryChooser();
         directoryChooser.setTitle("Select Existing Project");
         File selectedDirectory = directoryChooser.showDialog(null);
@@ -65,6 +69,7 @@ public class WelcomeController {
     @FXML
     public void quit(ActionEvent actionEvent) {
         // Close the window
+        tts.speak("Quit");
         System.exit(0);
     }
 }
